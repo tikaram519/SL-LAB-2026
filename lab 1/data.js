cf5ebd801f284be19bb50fde22a569bb2c738181
tikaram2024 <tikaramj519@gmail.com>
2026-06-09 11:13:23 +0545

    lab 1

---
+++ lab 1/data.js
@@ -0,0 +1,11 @@
+const countries = ["India", "Usa", "Austrialia", "Japan"];
+const dataList =  document.getElementById("dataList");
+
+for(name of countries) 
+{
+    dataList.innerHTML += `<li>${name}</li>`;
+}
+{
+    dataList.innerHTML += `<ul> ${name}</ul>`;
+}
+
